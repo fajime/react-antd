99b8572b4cce735f154a71ffaaf4443a6fc93d4a
fajime <jm-francisco@hotmail.com>
2024-12-02 09:08:08 +0100

    prueba

--- src/ant-modules/index.ts
+++ src/ant-modules/index.ts
@@ -1,5 +1,5 @@
-import { Anchor, Col, Form, Layout, notification, Row, Table, Tooltip } from 'antd';
-export { Anchor, Col, Form, Layout, notification, Row, Table, Tooltip };
+// import { Anchor, Col, Form, Layout, notification, Row, Table, Tooltip } from 'antd';
+export { Anchor, Col, Form, Layout, Row, Table, Tooltip, notification };
 
 export const { Header, Content, Footer, Sider } = Layout;
 export const { Link } = Anchor;
